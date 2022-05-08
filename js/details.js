00fc213a56dd5e3ab1d391afe52fb3230d12b84b
olem90 <olemariusrognan90@gmail.com>
2022-05-08 13:15:56 +0200

    final update

--- js/details.js
+++ js/details.js
@@ -8,6 +8,8 @@ const params = new URLSearchParams(queryString);
 
 const id = params.get("id");
 
+const message = "Sorry, we do not have a website available";
+
 console.log(id);
 
 const url = "https://api.openbrewerydb.org/breweries/" + id;
@@ -31,6 +33,10 @@ async function fetchBrewery(){
 }
 
 function createHTML(details){
+
+    if(details.website_url === null) {
+        details.website_url = message;
+    }         
     
     detailsContainer.innerHTML = 
                                   `<h1 class="company-name">${details.name}</h1>
@@ -39,9 +45,10 @@ function createHTML(details){
                                   <div>Country - ${details.country}</div>
                                   <div>State - ${details.state}</div>
                                   <div id="website">Website - <a href="${details.website_url}">${details.website_url}</a></div>
-                                  </div>`;
-                          
-}
+                                  </div>`;              
+}           
+
 fetchBrewery();
 
-// I wanted to make a message displaying that a website is not available, if website = null, and remove the a tag. But I didnt find a way to do it. I would really appreciate it if you could tell me how, if that is possible.
+// I wanted to remove the lnk if the brewery didnt have a website. But I didnt find a way to do it. I would really appreciate it if you could tell me how, if that is possible.
+
